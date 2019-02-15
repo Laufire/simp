12eb99ac75dd11f65e5a6a9be4d99eabc17c3892
Viswanath Chidambaram <viswanc@thoughtworks.com>
2019-02-16 00:49:28 +0530

    Started serving static sites.

--- config/default.js
+++ config/default.js
@@ -7,5 +7,7 @@ module.exports = {
   SSLPaths: {
     key: 'config/ssl/privkey.pem', // #Note: The files names are the defaults of the SSL certificates provided Let's encrypt.
     cert: 'config/ssl/cert.pem',
-  }
+  },
+
+  Sites: {},
 }
--- index.js
+++ index.js
@@ -1,19 +1,59 @@
 const fs = require('fs');
+
+const express = require('express');
+
 const DefaultConfig = require('./config/default');
+const app = express();
 
 module.exports = new function() {
 
   const self = this;
-
   const Server = {
     Options: {}
   };
 
-  this.start = (configExtensions = {}) => {
+  const normalizeConfig = (ConfigExtensions) => {
 
-    const Config = Object.assign({}, DefaultConfig, configExtensions);
+    let Config = Object.assign({}, DefaultConfig, ConfigExtensions);
+    let Sites = Config.Sites;
+
+    for(let [siteName, Site] of Object.entries(Sites)) {
+      
+      let aliasOf = Site.aliasOf;
+
+      if(typeof(aliasOf) !== 'undefined') {
+        Sites[siteName] = Object.assign(Site, Sites[aliasOf]);
+      }
+    }
+
+    return Config;
+  }
+
+  const setupApp = (Config) => {
+
+    let Sites = Config.Sites;
+
+    for(let [siteName, Site] of Object.entries(Sites)) {
+      
+      if(Site.static && typeof(Site.aliasOf) === 'undefined') { // Setup static sites.
+        
+        app.use(siteName ? ('/' + siteName) : '', express.static(Site.dir));
+      }
+    }
+
+    app.all('*', function (req, res) {
+      
+      console.log(req.url);
+      res.write(req.url);
+      res.statusCode = 200;
+      res.end();
+    });
+  }
+
+  this.start = (ConfigExtensions = {}) => {
 
     let Options = Server.Options;
+    let Config = normalizeConfig(ConfigExtensions)
 
     if(Config.https) {
       Options.key = fs.readFileSync(Config.SSLPaths.key);
@@ -28,15 +68,9 @@ module.exports = new function() {
       Server.createServer = require('http').createServer;
     }
 
-    Server.port = configExtensions.port || Server.port;
+    Server.port = ConfigExtensions.port || Server.port;
 
-    Server.createServer(function (req, res) {
-      console.log(req.url);
-      
-      res.write('');
-      res.statusCode = 200;
-      res.end();
-      
-    }).listen(Server.port);
+    setupApp(Config);
+    Server.createServer(app).listen(Server.port);
   }
 }
