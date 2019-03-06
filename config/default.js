c55a63917c63687ce3db4b9c54b2f6ebfa9e0cda
Viswanath Chidambaram <viswanc@thoughtworks.com>
2019-03-06 23:18:01 +0530

    Changed the default config to resemble, simp-server.

--- config/default.js
+++ config/default.js
@@ -3,11 +3,11 @@ module.exports = {
   https: true,
 
   SSLPaths: {
-    key: 'config/ssl/privkey.pem', // #Note: The files names are the defaults of the SSL certificates provided Let's encrypt.
-    cert: 'config/ssl/cert.pem',
+    key: './data/ssl/privkey.pem', // #Note: The files names are the defaults of the SSL certificates provided Let's encrypt.
+    cert: './data/ssl/cert.pem',
   },
 
-  sitesDir: './sites', // #Note: This has to be a relative path to the CWD.
+  sitesDir: './data/sites', // #Note: This has to be a relative path to the CWD.
 
   Sites: {
 
