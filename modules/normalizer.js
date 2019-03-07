b9eea1a8f3ab315045cf7150285854ae7d58efa2
Viswanath Chidambaram <viswanc@thoughtworks.com>
2019-03-08 00:03:18 +0530

    Bug fixed: Infering port config wasn't working properly.

--- modules/normalizer.js
+++ modules/normalizer.js
@@ -3,27 +3,40 @@ const { iterateObject } = require('../modules/utils');
 
 module.exports = new function() {
 
-  this.normalizeConfig = (ConfigExtensions) => {
+  self = this;
 
-    const Config = Object.assign({},
-      DefaultConfig,
-      {
-        port: ConfigExtensions.https ? 443 : 80
-      },
-      ConfigExtensions
-    );
+  this.addDefaultConfig = (Config) => Object.assign({},
+    DefaultConfig,
+    Config
+  );
+
+  this.normalizeSites = (Config) => {
     const Sites = Config.Sites;
     const DefaultSiteValues = {
       type: 'static',
     };
 
     iterateObject(Sites, (siteName, Site) => (
-      
+
       Sites[siteName] = Object.assign({}, DefaultSiteValues, Site, {
         name: siteName,
         dir: `${process.cwd()}/${Site.dir || (Config.sitesDir + '/' + siteName)}`
       })
     ));
+  }
+
+  this.addInferedConfig = (Config) => {
+    if(typeof Config.port === 'undefined') {
+      Config.port = Config.https ? 443 : 80
+    }
+  }
+
+  this.normalizeConfig = (ConfigExtensions) => {
+
+    const Config = self.addDefaultConfig(ConfigExtensions);
+
+    self.normalizeSites(Config);
+    self.addInferedConfig(Config);
 
     return Config;
   }
